280212eb330118627b42f39d23c457cd54fa5a2b
zin mar oo <zinmaroo@securelinkmm.com>
2022-11-16 17:23:33 +0630

    test

--- Dept_Labour_Immi/Models/ThaiCompany.cs
+++ Dept_Labour_Immi/Models/ThaiCompany.cs
@@ -1,10 +1,12 @@
-﻿namespace Dept_Labour_Immi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dept_Labour_Immi.Models
 {
     public class ThaiCompany
     {
         public int ID { get; set; }
+        [Required]
+        [Display(Name = "ထိုင်းကုမ္ပဏီအမည်")]
         public string CompanyName { get; set; }
-
-        public List<Agency> agencies { get; set; }
     }
 }
